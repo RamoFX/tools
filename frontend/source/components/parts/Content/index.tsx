52f248044fd7dad9ddc6b4da6ecb3cfe6f05f26c
ramofx <roman.dvorovoy123@gmail.com>
2021-04-01 11:08:21 +0200

    fix: added component div container

--- frontend/source/components/parts/Content/index.tsx
+++ frontend/source/components/parts/Content/index.tsx
@@ -4,17 +4,19 @@ import LoadingPage from '@components/pages/Loading'
 import { routes } from '@components/parts/Nav/routes'
 
 const Content = () => (
-  <Suspense fallback={ LoadingPage }>
-    <Switch>
-      {
-        routes.map(({ path, name: _, component: Component }, idx) => (
-          <Route exact={ idx == 0 } path={ path } key={ idx }>
-            <Component />
-          </Route>
-        ))
-      }
-    </Switch>
-  </Suspense>
+  <div className='part-content'>
+    <Suspense fallback={ LoadingPage }>
+      <Switch>
+        {
+          routes.map(({ path, name: _, component: Component }, idx) => (
+            <Route exact={ idx == 0 } path={ path } key={ idx }>
+              <Component />
+            </Route>
+          ))
+        }
+      </Switch>
+    </Suspense>
+  </div>
 )
 
 export default Content
