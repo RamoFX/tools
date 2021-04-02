025cb6fd52d0c878ab3d47f1ae3d37abc4f6da68
ramofx <roman.dvorovoy123@gmail.com>
2021-04-02 15:00:43 +0200

    update: added basic styles

--- frontend/source/index.tsx
+++ frontend/source/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { render } from 'react-dom'
+import './index.scss'
 import MainLayout from '@layouts/Main'
 
 render(<MainLayout />, document.getElementById('root'))
