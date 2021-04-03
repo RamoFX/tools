f35a89671c2e63c93a432881c18abb26c92b6f9b
ramofx <roman.dvorovoy123@gmail.com>
2021-04-03 21:55:07 +0200

    update: optional property

--- frontend/source/components/elements/Icon/index.tsx
+++ frontend/source/components/elements/Icon/index.tsx
@@ -5,10 +5,10 @@ export enum IconSize { VerySmall, Small, Medium, Large, VeryLarge  }
 
 interface IconProperties {
   path: string,
-  size: IconSize
+  size?: IconSize
 }
 
-const Icon: FC<IconProperties> = ({ path, size }) => (
+const Icon: FC<IconProperties> = ({ path, size = IconSize.Medium }) => (
   <div className={ `element-icon size-${ IconSize[size]!.toLowerCase() }` }>
     <svg viewBox='0 0 24 24'>
       <g>
