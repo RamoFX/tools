83b2aa30a8311c1275efa205387221d76221a384
ramofx <roman.dvorovoy123@gmail.com>
2021-04-04 00:26:36 +0200

    implementation: home page component

--- frontend/source/components/pages/Home/index.tsx
+++ frontend/source/components/pages/Home/index.tsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { MouseEventHandler } from 'react'
+import { useHistory } from 'react-router-dom'
+import TitlesContainer from '@containers/Titles'
+import PageContainer from '@containers/Page'
+import ButtonElement, { ButtonType, ButtonSize } from '@elements/Button'
 
-const Home = () => (
-  <div className='page-home'>
-    Home Page
-  </div>
-)
+const Home = () => {
+  const history = useHistory()
+
+  const clickHandler: MouseEventHandler<HTMLButtonElement> = () => {
+    history.push('/tools')
+  }
+
+  return (
+    <PageContainer className='page-home text-center rows ai-c jc-c'>
+      <TitlesContainer>
+        <h1 className='font-weight-bold'>Tools for development</h1>
+        <h2>Fast &amp; easy!</h2>
+      </TitlesContainer>
+      <ButtonElement text="Let's start" type={ ButtonType.Elevatable } clickHandler={ clickHandler } />
+    </PageContainer>
+  )
+}
 
 export default Home
