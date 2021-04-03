630c135f9fe59e1738dcc4ef7e18564c7fbb572f
ramofx <roman.dvorovoy123@gmail.com>
2021-04-03 11:38:29 +0200

    optimized: simplified

--- frontend/source/components/parts/Content/index.tsx
+++ frontend/source/components/parts/Content/index.tsx
@@ -1,21 +1,16 @@
-import React, { Suspense } from 'react'
+import React from 'react'
 import { Switch, Route } from 'react-router-dom'
-import LoadingPage from '@pages/Loading'
 import { routes } from '@parts/Nav/routes'
 
 const Content = () => (
   <div className='part-content'>
-    <Suspense fallback={ LoadingPage }>
-      <Switch>
-        {
-          routes.map((route, idx) => (
-            <Route exact={ idx == 0 } path={ route.main.path } key={ idx }>
-              <route.main.component />
-            </Route>
-          ))
-        }
-      </Switch>
-    </Suspense>
+    <Switch>
+      {
+        routes.map((route, idx) => (
+          <Route exact={ true } path={ route.main.path } key={ idx } component={route.main.component} />
+        ))
+      }
+    </Switch>
   </div>
 )
 
